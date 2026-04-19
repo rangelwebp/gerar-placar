63d2101a81a34521fac4b2281b3d265a5a5f8416
Rangel Xavier <rangel.webp@gmail.com>
2026-04-19 20:31:41 -0300

    Update page.js

--- src/app/preview/page.js
+++ src/app/preview/page.js
@@ -160,7 +160,7 @@ export default function PreviewPage() {
 							<img
 								src={homeTeam.logo}
 								alt={homeTeam.name}
-								className="max-h-24 w-20 max-w-[188px] object-contain"
+								className="max-h-[188px] max-w-[188px] w-20  object-contain"
 							/>
 						</div>
 
@@ -193,7 +193,7 @@ export default function PreviewPage() {
 							<img
 								src={awayTeam.logo}
 								alt={awayTeam.name}
-								className="max-h-24 w-20 max-w-[188px] object-contain"
+								className="max-h-[188px] max-w-[188px] w-20 object-contain"
 							/>
 						</div>
 					</div>
